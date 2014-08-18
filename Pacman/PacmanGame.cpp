aa308b3b4f6545e07ff0afcfe4429648d9b18532
Nikolay Dionisov <nikolaydionisov@gmail.com>
2014-08-18 09:42:01 +0300

    Pacman movement + food

--- Pacman/PacmanGame.cpp
+++ Pacman/PacmanGame.cpp
@@ -21,6 +21,11 @@ uint8_t char_to_tile(char ch) {
 	}
 	return 255;
 }
+bool is_walkable(uint8_t ch) {
+	if(ch == TT_BLOCK)
+		return false;
+	return true;
+}
 
 TileMap* LoadTileMap(const char* filename) {
 	std::ifstream file(filename);
@@ -74,37 +79,110 @@ TileMap* LoadTileMap(const char* filename) {
 
 PacmanGame::PacmanGame() {
 	tile_map = 0;
+	tile_size = 20;
 }
 void PacmanGame::Init(ResourceManager& res_manager) {
 	tiles[TT_BLOCK].tex = res_manager.GetTexture("block-temp.png");
 	tiles[TT_EMPTY].tex = res_manager.GetTexture("empty-tile1.png");
-	tiles[TT_FOOD].tex = res_manager.GetTexture("empty-tile1.png");
+	tiles[TT_FOOD].tex = res_manager.GetTexture("food.png");
 	tiles[TT_PACMAN].tex = res_manager.GetTexture("empty-tile1.png");
 	tiles[TT_GHOST].tex = res_manager.GetTexture("empty-tile1.png");
+	sprite_sheet = res_manager.GetTexture("sprite_sheet.png");
 }
 void PacmanGame::SetTilemap(TileMap* map) {
 	delete tile_map;
 	tile_map = map;
+	//this is also the "reset" function
+	pacman.current_state = 0;
+	pacman.ori = Vector2df(-1, 0);
+	pacman.next_ori = pacman.ori;
+	pacman.sheet_offset = Vector2d(96, 0);
+	pacman.sheet_stride = Vector2d(0, 16 * 2);
+	pacman.sprite_size = Vector2d(15, 16);
+	
+	Vector2d tile = tile_map->Search(TT_PACMAN);
+	pacman.position = Vector2df(tile.x * tile_size, tile.y * tile_size);
+	pacman.target_pos = pacman.position;
 }
 
 void PacmanGame::Update(float dt, uint32_t input_state, uint32_t input_events, PacmanApp* app) {
 	if(input_events & INPUT_ESC) {
 		app->LoadStartScreen();
+		return;
 	}
-	
+	if(input_state & INPUT_LEFT) {
+		pacman.next_ori.x = -1;
+		pacman.next_ori.y = 0;
+	}else if(input_state & INPUT_RIGHT) {
+		pacman.next_ori.x = 1;
+		pacman.next_ori.y = 0;
+	}else if(input_state & INPUT_UP) {
+		pacman.next_ori.y = -1;
+		pacman.next_ori.x = 0;
+	}else if(input_state & INPUT_DOWN) {
+		pacman.next_ori.y = 1;
+		pacman.next_ori.x = 0;
+	}
+
+	Vector2df pacman_center = pacman.position + Vector2df(tile_size / 2, tile_size / 2);
+	Vector2df pacman_tile = pacman_center * (1.0 / tile_size);
+	pacman_tile.x = floor(pacman_tile.x);
+	pacman_tile.y = floor(pacman_tile.y);
+
+	//pacman movement code
+	if(pacman.target_pos.x == pacman.position.x
+		&& pacman.target_pos.y == pacman.position.y) {
+
+			Vector2df next_tile = pacman_tile + pacman.next_ori;
+			if(is_walkable(tile_map->GetTile(next_tile.x, next_tile.y))) {
+				pacman.target_pos = next_tile * tile_size;
+				pacman.ori = pacman.next_ori;
+			}else{
+				next_tile = pacman_tile + pacman.ori;
+				if(is_walkable(tile_map->GetTile(next_tile.x, next_tile.y))) {
+					pacman.target_pos = next_tile * tile_size;
+				}
+			}
+	}
+
+	//food collection
+	if(tile_map->GetTile(pacman_tile.x, pacman_tile.y) == TT_FOOD) {
+		tile_map->SetTile(pacman_tile.x, pacman_tile.y, TT_EMPTY);
+	}
+
+
+	Vector2df velo = pacman.target_pos - pacman.position;
+	float dist = velo.len();
+	pacman.position = pacman.position + velo.normalized() * std::min(dist, 100 * dt);
 }
 void PacmanGame::Render(float dt, SDL_Renderer* renderer) {
-	if(!tile_map) {
+	if(!tile_map) { 
 		return;
 	}
 	SDL_Rect rect;
+	SDL_Rect src;
 	for(int x = 0; x < tile_map->GetWidth(); ++x) {
 		for(int y = 0; y < tile_map->GetHeight(); ++y) {
-			rect.x = x * 20;
-			rect.y = y * 20;
-			rect.w = 20;
-			rect.h = 20;
+			rect.x = x * tile_size;
+			rect.y = y * tile_size;
+			rect.w = tile_size;
+			rect.h = tile_size;
 			SDL_RenderCopy(renderer, tiles[tile_map->GetTile(x, y)].tex.ptr, 0, &rect);
 		}
 	}
+	pacman.current_state += dt * 5;
+	if(pacman.current_state > 3) {
+		pacman.current_state = pacman.current_state - 6;
+	}
+	Vector2d src_pos = pacman.sheet_offset + pacman.sheet_stride * (int)abs(pacman.current_state);
+	src.x = src_pos.x;
+	src.y = src_pos.y;
+	src.w = pacman.sprite_size.x;
+	src.h = pacman.sprite_size.y;
+	rect.x = pacman.position.x;
+	rect.y = pacman.position.y;
+	rect.w = tile_size;
+	rect.h = tile_size;
+
+	SDL_RenderCopy(renderer, sprite_sheet.ptr, &src, &rect);
 }
--- Pacman/PacmanGame.h
+++ Pacman/PacmanGame.h
@@ -3,6 +3,7 @@
 #include <cassert>
 #include "GameLogic.h"
 #include "Graphics.h"
+#include <cmath>
 
 
 enum TILE_TYPE {
@@ -12,6 +13,45 @@ enum TILE_TYPE {
 	TT_PACMAN,
 	TT_GHOST,
 	TT_TILE_COUNT
+};
+struct Vector2df {
+	Vector2df() {}
+	Vector2df(float x_, float y_) : x(x_), y(y_) {}
+	Vector2df operator+(const Vector2df& sec) const {
+		return Vector2df(x + sec.x, y + sec.y);
+	}
+	Vector2df operator-(const Vector2df& sec) const {
+		return Vector2df(x - sec.x, y - sec.y);
+	}
+	Vector2df operator*(float factor) const {
+		return Vector2df(x * factor, y * factor);
+	}
+	float len() const {
+		return sqrt(x*x + y*y);
+	}
+	Vector2df normalized() const {
+		float l = len();
+		if(l == 0)
+			l = 1;
+		return Vector2df(x / l, y / l);
+	}
+
+	float x, y;
+};
+struct Vector2d {
+	Vector2d() {}
+	Vector2d(const Vector2df& v) : x(v.x), y(v.y) {}
+	Vector2d(int x_, int y_) : x(x_), y(y_) {}
+	Vector2d operator+(const Vector2d& sec) const {
+		return Vector2d(x + sec.x, y + sec.y);
+	}
+	Vector2d operator*(int factor) const {
+		return Vector2d(x * factor, y * factor);
+	}
+	Vector2df operator*(float factor) const {
+		return Vector2df(x * factor, y * factor);
+	}
+	int x, y;
 };
 class TileMap {
 	uint8_t* tiles_;
@@ -39,18 +79,47 @@ public:
 		assert(y >= 0 && y < height_);
 		tiles_[x + y * width_] = tile;
 	}
+	Vector2d Search(uint8_t tile) {
+		Vector2d pos;
+		for(pos.x = 0; pos.x < width_; ++pos.x) {
+			for(pos.y = 0; pos.y < height_; ++pos.y) {
+				if(GetTile(pos.x, pos.y) == tile) 
+					return pos;
+			}
+		}
+		return pos;
+	}
 	int GetWidth() { return width_; }
 	int GetHeight() { return height_;}
 };
 
 TileMap* LoadTileMap(const char* filename);
 
+enum ORIENTATION {
+	UP, DOWN, LEFT, RIGHT
+};
 class PacmanGame : public GameLogic {
 	TileMap* tile_map;
+	Texture sprite_sheet;
 	struct TileData {
 		Texture tex;
 	};
+	struct Pacman {
+		Vector2d sheet_offset;
+		Vector2d sheet_stride;
+		Vector2d sprite_size;
+		float current_state;
+
+		Vector2df position;
+		Vector2df target_pos;
+		//the current way pacaman is going
+		Vector2df ori;
+		//next one
+		Vector2df next_ori;
+	};
 	TileData tiles[TT_TILE_COUNT];
+	int tile_size;
+	Pacman pacman;
 public:
 
 	PacmanGame();
